edfdc82ba6e8cd23523395a50891cc7c7789826d
Kolimaro <saithikus@gmail.com>
2020-04-18 23:46:33 +0800

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,18 +1,14 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.support.RequestPartServletServerHttpRequest;
-import web.DAO.CarDao;
 import web.models.Car;
+import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,11 +16,11 @@ import java.util.List;
 public class CarController {
 
     @Autowired
-    private CarDao carDao;
+    private CarService carService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(@RequestParam(value = "locale", required = false) String locale, ModelMap model) {
-        List<Car> cars = carDao.allCars();
+        List<Car> cars = carService.allCars();
         if (locale != null) {
             if (locale.equals("ru")) {
                 model.addAttribute("locale", "МАШИНЫ");
